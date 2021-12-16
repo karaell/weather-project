167e27c00cc3bb63ac8c0cdb5b4fa12449308f82
karaell <elenakara2@gmail.com>
2021-12-16 21:52:22 +0300

    weather

---
+++ js/main.js
@@ -0,0 +1,22 @@
+import {UI} from './view.js'
+
+UI.BTN_SEARCH.addEventListener('submit', function () {
+    const serverUrl = 'http://api.openweathermap.org/data/2.5/weather';
+    const cityName = UI.INPUT_SEARCH.value;
+    const apiKey = 'f660a2fb1e4bad108d6160b7f58c555f';
+    const url = `${serverUrl}?q=${cityName}&appid=${apiKey}&units=metric`;
+
+
+	
+  fetch(url)
+  	.then(result =>  result.json() )
+    .then(res => {
+        console.log(res)
+        UI.LOCATION_NAME.textContent = `${res.name}`;
+        UI.LOCATION_TEMPERATURE.textContent = `${res.main.temp + '°'}`;
+    })
+    UI.INPUT_SEARCH.value = ""
+
+  
+  
+})
---
+++ js/view.js
@@ -0,0 +1,7 @@
+export const UI = {
+	INPUT_SEARCH: document.querySelector(".input-search"),
+	BTN_SEARCH: document.querySelector(".btn-search"),
+    LOCATION_NAME: document.querySelector(".now-location"),
+    LOCATION_TEMPERATURE: document.querySelector(".temperature"),
+    WEATHER_ICON: document.querySelector(".icon-weather")
+}
